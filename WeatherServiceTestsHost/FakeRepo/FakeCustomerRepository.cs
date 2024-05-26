289f17aabf1a9f66ee2ac0a876dfce4b45e4ea36
Anthony Steele <Anthony@PlasticAvalon.org>
2024-05-26 14:56:55 +0100

    updates

--- WeatherServiceTestsHost/FakeRepo/FakeCustomerRepository.cs
+++ WeatherServiceTestsHost/FakeRepo/FakeCustomerRepository.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestsWithHost.FakeRepo;
+﻿namespace TestsWithHost.FakeRepo;
 
 // demo of the basic technique
 // of an "in memory fake database"
 // a lightwitght way to fake a dependency
+// the type and interface defintions are stubs
+// to show how the fake repository would work
 
-public record Customer(int Id, DateOnly DateOfBirth, string Name);
+public record Customer(int Id, DateOnly DateOfBirth, string Name, string Address, bool active);
 
+/// <summary>
+/// Define some basic Create, Read, Update operations on the customer type
+/// </summary>
 public interface ICustomerRepository
 {
-    public Customer? Get(int id);
     public void Add(Customer customer);
+    public Customer? Get(int id);
+    public bool Activate(int id);
 }
 
 public class FakeCustomerRepository : ICustomerRepository
@@ -26,13 +26,31 @@ public class FakeCustomerRepository : ICustomerRepository
         => _data.Add(customer);
 
     public Customer? Get(int id) 
-        => _data.FirstOrDefault(c => c.Id == id);
+        => _data.SingleOrDefault(c => c.Id == id);
+
+    public bool Activate(int id)
+    {
+        var matchingCustomer = Get(id);
+        if (matchingCustomer != null)
+        {
+            var update = matchingCustomer with { active = true };
+            _data.Remove(matchingCustomer);
+            _data.Add(update);
+            return true;
+        }
+
+        return false;
+    }
 
     /// <summary>
-    /// Non-intwerfaced methods can be used in test 
-    /// To verify that the expected dta has been sent to the data store
+    /// Non-interfaced methods can be used in tests 
+    /// To verify that the expected data is in the data store
     /// </summary>
     /// <returns></returns>
     public IReadOnlyCollection<Customer> GetAll()
         => _data;
+
+    public bool Any(Func<Customer, bool> predicate)
+        => _data.Any(predicate);
+
 }
